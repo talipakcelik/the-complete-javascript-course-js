c295c8b11b72b91f3c472b5c6d3f605fc59772d6
Talip <talipakcelik@hacettepe.edu.tr>
2021-12-12 17:46:12 +0300

    036 is comp.

--- 03-javascript-fundemantals-part-2/script.js
+++ 03-javascript-fundemantals-part-2/script.js
@@ -13,20 +13,20 @@ if (hasDriversLicense) console.log("Araba sürebilirim.");
  *****033 Functions****
  **********************/
 
-function meyveİmalatı(elma, portakal) {
-  const meyveSuyu = `${elma} elmalı meyvesuyu ve ${portakal}
-  portakallı meyvesuyu.`;
-  return meyveSuyu;
-}
+// function meyveİmalatı(elma, portakal) {
+//   const meyveSuyu = `${elma} elmalı meyvesuyu ve ${portakal}
+//   portakallı meyvesuyu.`;
+//   return meyveSuyu;
+// }
 
 /* burada fonksiyonu tanımladık. çalıştırmadık. */
 
 
 
 
-const yeniMeyveSuyu = meyveİmalatı(3, 4);
-console.log(yeniMeyveSuyu)
-console.log(meyveİmalatı(3, 8))
+// const yeniMeyveSuyu = meyveİmalatı(3, 4);
+// console.log(yeniMeyveSuyu)
+// console.log(meyveİmalatı(3, 8))
 
 /**********************
  *****033 Functions****
@@ -49,3 +49,50 @@ const YaşHesaplama2 = function (doğumyılı) {
 const yaş2 = YaşHesaplama2(1995);
 
 console.log(yaş1, yaş2)
+
+
+/**********************
+ *****034 Functions****
+ **********************/
+
+
+const YaşOrtalaması3 = dogumyili => 2037 - dogumyili;
+const yas3 = YaşOrtalaması3(2001);
+console.log(yas3)
+
+
+// YaşOrtalaması3(2002);
+// console.log(YaşOrtalaması3(2002))
+
+// const emeklilikHesaplama = dogumyili => {
+//   const yas = 2037 - dogumyili;
+//   const emeklilik = 65 - yas;
+//   return emeklilik;
+// }
+
+// console.log(emeklilikHesaplama(1995));
+
+///
+
+const emeklilikHesaplama = (dogumYili, isim) => {
+  const yas = 2037 - dogumYili;
+  const emeklilik = 65 - yas;
+  return `${isim}, ${emeklilik} yıl sonra emekli olacak.`
+}
+
+console.log(emeklilikHesaplama(1995, "Talip"))
+
+
+///
+const meyveDilimle = function (fruit) {
+  return fruit * 4;
+}
+
+function meyveImalati(elma, portakal) {
+  const elmaDilimleri = meyveDilimle(elma);
+  const portakalDilimleri = meyveDilimle(portakal);
+  const meyveSuyu = `${elmaDilimleri} dilimli elmalı meyvesuyu ve ${portakalDilimleri} dilimli portakallı meyvesuyu.`;
+  return meyveSuyu;
+}
+
+console.log(meyveImalati(4, 2))
