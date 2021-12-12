0a277f56ea3f1f4bf06e85a809d4445796100941
Talip <talipakcelik@hacettepe.edu.tr>
2021-12-12 15:31:33 +0300

    033 is comp.

--- 03-javascript-fundemantals-part-2/script.js
+++ 03-javascript-fundemantals-part-2/script.js
@@ -6,7 +6,24 @@
 let hasDriversLicense = false;
 const passTest = true;
 
-if (passTest) hasDriverLicense = true;
-if (hasDriverLicense) console.log("Araba sürebilirim.");
+if (passTest) hasDriversLicense = true; /*normalde s yok*/
+if (hasDriversLicense) console.log("Araba sürebilirim.");
+
+/**********************
+ *****033 Functions****
+ **********************/
+
+function meyveİmalatı(elma, portakal) {
+  const meyveSuyu = `${elma} elmalı meyvesuyu ve ${portakal}
+  portakallı meyvesuyu.`;
+  return meyveSuyu;
+}
+
+/* burada fonksiyonu tanımladık. çalıştırmadık. */
 
 
+
+
+const yeniMeyveSuyu = meyveİmalatı(3, 4);
+console.log(yeniMeyveSuyu)
+console.log(meyveİmalatı(3, 8))
