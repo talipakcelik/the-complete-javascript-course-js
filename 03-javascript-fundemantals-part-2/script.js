fc5088d9816294ea1fb9aa46400f16b51bc6bf72
Talip <talipakcelik@hacettepe.edu.tr>
2021-12-12 13:37:53 +0300

    032

---
+++ 03-javascript-fundemantals-part-2/script.js
@@ -0,0 +1,12 @@
+/****************
+ ******032******
+ ************///
+"use strict";
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true;
+if (hasDriverLicense) console.log("Araba sürebilirim.");
+
+
