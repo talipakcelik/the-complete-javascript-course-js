51347febf79d5f7a8459d8f93aba35a86ca7b73d
Talip <talipakcelik@hacettepe.edu.tr>
2021-12-19 17:02:47 +0300

    js-f-2 c_c_4 tamamlandı

--- tekrarlar/tekrar01.js
+++ tekrarlar/tekrar01.js
@@ -83,3 +83,40 @@ const winner = function () {
     markBMI.fullName
   }'ın (${markBMI.calcBMI()}) değerinden daha yüksek `);
 };
+
+/////////////////////////////
+/// Coding Challenge #4/////
+////////////////////////////
+
+const calcTip3 = function (bills) {
+  return bills >= 50 && bills <= 300 ? bills * 0.15 : bills * 0.2;
+}; // ternary operator ile
+
+const bills2 = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips2 = [];
+const total2 = [];
+
+for (let i = 0; i < bills2.length; i++) {
+  tips2.push([calcTip3(bills2[i])]);
+  total2.push(Number(bills2[i]) + Number(tips2[i]));
+  console.log(
+    `bill ${i} = ${bills2[i]}, 
+  tip ${i} = ${tips2[i]}, 
+  total ${i} = ${total2[i]}`
+  );
+}
+
+// bonus
+
+const calcAverage2 = function (arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    // sum = sum + arr
+    sum += arr[i];
+    console.log(sum);
+  }
+  return sum / arr.length;
+  //console.log(`${sum / arr.length}`);
+};
+
+console.log(calcAverage2(total2));
