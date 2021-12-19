b4ece17deca70883b30cec719c89d6fe2e12125e
Talip <talipakcelik@hacettepe.edu.tr>
2021-12-19 14:48:05 +0300

    js-fundemantals-2 c_c_2 tamamlandı

--- tekrarlar/tekrar01.js
+++ tekrarlar/tekrar01.js
@@ -27,3 +27,27 @@ checkWinner(ortDolphins, ortKoalas);
 ortDolphins = calcAverage(85, 54, 41);
 ortKoalas = calcAverage(23, 34, 27);
 console.log(ortDolphins, ortKoalas);
+
+/////////////////////////////
+/// Coding Challenge #2/////
+////////////////////////////
+
+const calcTip = function (bills) {
+  if (bills >= 50 && bills <= 300) {
+    return bills * 0.15;
+  } else {
+    return bills * 0.2;
+  }
+};
+
+const calcTip2 = function (bills) {
+  return bills >= 50 && bills <= 300 ? bills * 0.15 : bills * 0.2;
+}; // ternary operator ile
+
+console.log(calcTip(100));
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log(bills, tips, total);
