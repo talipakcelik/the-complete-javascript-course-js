3fa3ab4cc86326ac6eda523499e8b8b9fdb6f160
Talip <talipakcelik@hacettepe.edu.tr>
2021-12-22 02:10:13 +0300

    074

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,19 +1,44 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
-///
-document.querySelector('.message').textContent = '🎉 Doğru Sayı ';
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-///
-document.querySelector('.guess').value = 99;
+// console.log(document.querySelector('.message').textContent);
+// ///
+// document.querySelector('.message').textContent = '🎉 Doğru Sayı ';
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+// ///
+// document.querySelector('.guess').value = 99;
 
 /////073 Handling Click Events
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1; // Math.trunc(Math.random() * 21) neden olmasın.
+document.querySelector('.number').textContent = secretNumber;
+
+let score = 20; // başlangıç değeri
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
   if (!guess) {
     document.querySelector('.message').textContent = '❌ Sayı girilmedi';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Doğru Sayı ';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Çok yüksek ';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = ' 💥 Oyunu kaybettiniz ';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Çok düşük ';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = ' 💥 Oyunu kaybettiniz ';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
