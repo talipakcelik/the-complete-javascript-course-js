1211bd8a4f829ab0792d75b5987011e47b31f803
Talip <talipakcelik@hacettepe.edu.tr>
2021-12-21 21:46:18 +0300

    072

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,3 +1,10 @@
 'use strict';
 
 console.log(document.querySelector('.message').textContent);
+///
+document.querySelector('.message').textContent = '🎉 Doğru Sayı ';
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+///
+document.querySelector('.guess').value = 99;
+console.log(document.querySelector('.guess').value);
