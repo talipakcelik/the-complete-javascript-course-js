f512db91e2e6dd2a6147f35119d1ec44d9e88175
Talip <talipakcelik@hacettepe.edu.tr>
2021-12-22 23:31:19 +0300

    081

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -21,6 +21,15 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
+document.addEventListener('keydown', function (e) {
+  // console.log('bir tuşa basıldı.');
+  // console.log(e);
+  console.log(e.key);
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
 // for (let i = 0; i < btnsOpenModal.length; i++) {
 //   btnsOpenModal[i].addEventListener('click', function () {
 //     modal.classList.remove('hidden');
