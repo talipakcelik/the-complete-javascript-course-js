46517e6ffdb9ef93c39da7ef877d62bf58baca8c
Talip <talipakcelik@hacettepe.edu.tr>
2021-12-23 15:26:58 +0300

    085

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -20,31 +20,70 @@ diceEl.classList.add('hidden');
 const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true;
+
+const switchPlayer = function () {
+  // sayı 1 gelirse geçişten önceki oyuncunun skoru 0 olsun
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  // diğer oyuncuya geçiş
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
 
 /// Zar atma functionality'si
 btnRoll.addEventListener('click', function () {
-  // 1. random zar atmayı hâlletme
-  const dice = Math.trunc(Math.random() * 6) + 1;
-  // 2. zarı göster
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${dice}.png`;
+  if (playing) {
+    // 1. random zar atmayı hâlletme
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    // 2. zarı göster
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
 
-  // 3. 1 gelip gelmediği kontrol:
-  if (dice !== 1) {
-    // current score'a zarı ekle
-    currentScore += dice;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
+    // 3. 1 gelip gelmediği kontrol:
+    if (dice !== 1) {
+      // current score'a zarı ekle
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
 
-    // current0El.textContent = currentScore;
-  } else {
-    // sayı 1 gelirse geçişten önceki oyuncunun skoru 0 olsun
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    // diğer oyuncuya geçiş
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+      // current0El.textContent = currentScore;
+    } else {
+      // sayı 1 gelirse geçişten önceki oyuncunun skoru 0 olsun
+      // document.getElementById(`current--${activePlayer}`).textContent = 0;
+      // currentScore = 0;
+      // diğer oyuncuya geçiş
+      // activePlayer = activePlayer === 0 ? 1 : 0;
+      // player0El.classList.toggle('player--active');
+      // player1El.classList.toggle('player--active');
+      switchPlayer();
+    }
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    // 1. current score'u active oyuncuya ekleme
+    scores[activePlayer] += currentScore; // scores[1] = scores[1] + currentScore; demek gibi
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    // 2. if oyuncunun skoru >= 100, kontrol et
+    if (scores[activePlayer] >= 25) {
+      // 2.1. oyunu bitir
+      playing = false;
+      diceEl.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      // 2.2. diğer oyuncuya geç
+      // else yani skor 100'e ulaşmamışsa diğer oyuncuya geç
+      switchPlayer();
+    }
   }
 });
 /***************************/
