f695a1b207e203451976be6b40de86420670050c
Talip <talipakcelik@hacettepe.edu.tr>
2021-12-23 03:00:19 +0300

    084

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 /// Elementleri seçme
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -15,7 +17,10 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
+
 /// Zar atma functionality'si
 btnRoll.addEventListener('click', function () {
   // 1. random zar atmayı hâlletme
@@ -28,10 +33,19 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // current score'a zarı ekle
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+
+    // current0El.textContent = currentScore;
   } else {
+    // sayı 1 gelirse geçişten önceki oyuncunun skoru 0 olsun
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    // diğer oyuncuya geçiş
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
-  // diğer oyuncuya geçiş
 });
 /***************************/
 //////////el yapımı/////////
