52e1a92ee90e4a1c3305821f7d760134ecabb3e4
Talip <talipakcelik@hacettepe.edu.tr>
2022-01-05 03:49:55 +0300

    Destructuring Objects is completed

---
+++ 09-Data-Structures-Operators/starter/script.js
@@ -0,0 +1,135 @@
+'use strict';
+
+const [a, b, c] = [2, 5, 6];
+
+const restaurant = {
+  categories: ['Italian', 'Pizzeria', 'Vegeterian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+};
+
+const [first, , second] = restaurant.categories;
+
+const [starter, main] = restaurant.order(2, 0);
+
+const restaurant2 = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+};
+//////////////////////
+const { name, openingHours, categories } = restaurant2;
+console.log(name, openingHours, categories);
+
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant2;
+console.log(restaurantName, hours, tags);
+
+const { menu = [], starterMenu: starters = [] } = restaurant2;
+console.log(menu, starters);
+
+///
+
+let t = 111;
+let l = 999;
+const obj = { t: 23, l: 7, p: 14 };
+///
+// const {a, b} = obj; // olmaz
+// let {a, b} = obj; // olmaz
+// {a, b} = obj; // error
+
+({ t, l } = obj);
+console.log(t, l);
+
+//////
+
+const { fri } = openingHours;
+console.log(fri);
+
+const {
+  fri: { open, close },
+} = openingHours;
+console.log(fri, open, close);
+
+///////
+
+const restaurant3 = {
+  categories: ['Italian', 'Pizzeria', 'Vegeterian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({ starterIndex, mainIndex, time, address }) {
+    console.log(`
+    Sipariş alındı! 
+    ${this.starterMenu[starterIndex]} ve ${this.mainMenu[mainIndex]} 
+    ${address} adresine, 
+    saat ${time}'da teslim edilecektir.
+    `);
+  },
+};
+
+restaurant3.orderDelivery({
+  time: '22:30',
+  address: 'bademlik mah. ankara',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+const restaurant4 = {
+  categories: ['Italian', 'Pizzeria', 'Vegeterian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({
+    starterIndex = 0,
+    mainIndex = 0,
+    time = '14:50',
+    address,
+  }) {
+    console.log(`
+    Sipariş alındı! 
+    ${this.starterMenu[starterIndex]} ve ${this.mainMenu[mainIndex]} 
+    ${address} adresine, 
+    saat ${time}'da teslim edilecektir.
+    `);
+  },
+};
+
+restaurant4.orderDelivery({
+  // time: '22:30',
+  address: 'bademlik mah. ankara',
+  // mainIndex: 2,
+  // starterIndex: 2,
+});
