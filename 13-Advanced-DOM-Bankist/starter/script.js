99403819dc635dbb225dd0ea74b24a85bd5f1f3c
Talip <talipakcelik@hacettepe.edu.tr>
2022-01-08 19:03:21 +0300

    Revealing Elements on Scroll

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -219,7 +219,7 @@ const navHeight = nav.getBoundingClientRect().height;
 console.log(navHeight);
 const stickyNav = function (entries) {
   const [entry] = entries;
-  console.log(entries);
+  // console.log(entries);
 
   if (!entry.isIntersecting) nav.classList.add('sticky');
   else nav.classList.remove('sticky');
@@ -230,3 +230,25 @@ const headerObserver = new IntersectionObserver(stickyNav, {
   rootMargin: `-${navHeight}px`,
 });
 headerObserver.observe(header);
+
+/// Revealing Elements on Scroll
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
